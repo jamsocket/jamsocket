d3fccc1aa18038d778768d78735066c2085c1194
Taylor Baldwin <taylorbaldwin@gmail.com>
2022-02-04 15:48:49 -0500

    add logout command

---
+++ src/commands/logout.ts
@@ -0,0 +1,18 @@
+import {Command} from '@oclif/core'
+
+export default class Logout extends Command {
+  static description = 'Logs out of jamcr.io container registry and removes locally-stored credentials.'
+
+  static examples = [
+    '<%= config.bin %> <%= command.id %>',
+  ]
+
+  static flags = {}
+  static args = []
+
+  public async run(): Promise<void> {
+    this.log('not implemented')
+    // docker logout jamcr.io
+    // remove locally-stored credentials
+  }
+}
