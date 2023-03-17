9356d4319127a2fd7dee47bd0e8164fa4b2cb3da
Taylor Baldwin <taylorbaldwin@gmail.com>
2023-03-17 14:54:59 +0000

    refactor jamsocket config to support api-tokens and user-sessions

--- src/jamsocket-config.ts
+++ src/jamsocket-config.ts
@@ -3,55 +3,101 @@ import { resolve, dirname } from 'path'
 import { existsSync, readFileSync, mkdirSync, writeFileSync, unlinkSync } from 'fs'
 
 export const JAMSOCKET_CONFIG_DIR = resolve(homedir(), '.jamsocket')
-const JAMSOCKET_CONFIG = resolve(JAMSOCKET_CONFIG_DIR, 'config.json')
+const JAMSOCKET_CONFIG_PATH = resolve(JAMSOCKET_CONFIG_DIR, 'config.json')
 
-export type JamsocketConfig = {
-  account: string;
-  token: string;
-}
-
-export function readJamsocketConfig(): JamsocketConfig | null {
-  if (!existsSync(JAMSOCKET_CONFIG)) return null
-  const contents = readFileSync(JAMSOCKET_CONFIG, 'utf-8')
-
-  let config
-  try {
-    config = JSON.parse(contents)
-  } catch {
-    deleteJamsocketConfig()
-    return null
-  }
-
-  const isInvalidConfig = !config.account || !config.token
-
-  if (isInvalidConfig) {
-    deleteJamsocketConfig()
-    return null
-  }
-
-  return {
-    account: config.account,
-    token: config.token,
+export type UserSessionConfig = {
+  user_session: {
+    uuid: string
+    token: string
+    user_id: string
+    primary_account: string
   }
 }
 
-export function writeJamsocketConfig(config: JamsocketConfig): void {
-  const dir = dirname(JAMSOCKET_CONFIG)
-  mkdirSync(dir, { recursive: true })
-  writeFileSync(JAMSOCKET_CONFIG, `${JSON.stringify(config, null, 2)}${EOL}`)
+export type ApiTokenConfig = {
+  api_token: {
+    account: string
+    token: string
+  }
+}
+
+export function isUserSessionConfig(config: any): config is UserSessionConfig {
+  return (
+    config !== undefined &&
+    config.user_session !== undefined &&
+    config.user_session.uuid !== undefined &&
+    config.user_session.token !== undefined &&
+    config.user_session.user_id !== undefined &&
+    config.user_session.primary_account !== undefined
+  )
+}
+
+export function isApiTokenConfig(config: any): config is ApiTokenConfig {
+  return (
+    config !== undefined &&
+    config.api_token !== undefined &&
+    config.api_token.token !== undefined &&
+    config.api_token.account !== undefined
+  )
 }
 
 export function deleteJamsocketConfig(): void {
-  if (!existsSync(JAMSOCKET_CONFIG)) return
-  unlinkSync(JAMSOCKET_CONFIG)
+  if (!existsSync(JAMSOCKET_CONFIG_PATH)) return
+  unlinkSync(JAMSOCKET_CONFIG_PATH)
 }
 
-// this version of the token is used for Basic Authorization, which is required for our Docker registry
-export function getRegistryAuth(account: string, token: string): string {
-  const buff = Buffer.from(`${account}:${token}`, 'utf-8')
-  return buff.toString('base64')
-}
+export class JamsocketConfig {
+  public static fromSaved(): JamsocketConfig | null {
+    if (!existsSync(JAMSOCKET_CONFIG_PATH)) return null
+    const contents = readFileSync(JAMSOCKET_CONFIG_PATH, 'utf-8')
 
-export function getTokenPublicPortion(token: string): string {
-  return token.split('.')[0]
+    let config
+    try {
+      config = JSON.parse(contents)
+    } catch {
+      deleteJamsocketConfig()
+      return null
+    }
+
+    const isInvalidConfig = isApiTokenConfig(config) === isUserSessionConfig(config)
+
+    if (isInvalidConfig) {
+      deleteJamsocketConfig()
+      return null
+    }
+
+    return new JamsocketConfig(config)
+  }
+
+  constructor(private config: UserSessionConfig | ApiTokenConfig) {}
+
+  save(): void {
+    const dir = dirname(JAMSOCKET_CONFIG_PATH)
+    mkdirSync(dir, { recursive: true })
+    writeFileSync(JAMSOCKET_CONFIG_PATH, `${JSON.stringify(this.config, null, 2)}${EOL}`)
+  }
+
+  getSessionUuid(): string | null {
+    if (isUserSessionConfig(this.config)) return this.config.user_session.uuid
+    return null
+  }
+
+  // returns a token that can access our API endpoints (either a user session token or api token)
+  getAccessToken(): string {
+    if (isApiTokenConfig(this.config)) return this.config.api_token.token
+    return this.config.user_session.token
+  }
+
+  getAccount(): string {
+    if (isApiTokenConfig(this.config)) return this.config.api_token.account
+    return this.config.user_session.primary_account
+  }
+
+  getRegistryAuth(): string {
+    // this version of the token is used for Basic Authorization, which is required for our Docker registry
+    const account = this.getAccount()
+    const token = this.getAccessToken()
+    const buff = Buffer.from(`${account}:${token}`, 'utf-8')
+    return buff.toString('base64')
+  }
 }
