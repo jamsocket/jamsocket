957a38e2081e178b91740c6b0368de86ab73955e
Taylor Baldwin <taylorbaldwin@gmail.com>
2024-04-02 11:20:40 -0400

    support jamsocket.config.json, support docker context via CLI flag, support -f flag for dockerfile

--- cli/src/commands/dev.ts
+++ cli/src/commands/dev.ts
@@ -1,10 +1,11 @@
-import { existsSync } from 'fs'
+import { existsSync, readFileSync } from 'fs'
 import path from 'path'
 import { Command, Flags } from '@oclif/core'
 import { createDevServer } from '../dev-server'
 import type { BuildImageOptions } from '../docker'
 
-const PROJECT_CONFIG_PATH = path.resolve(process.cwd(), 'jamsocket.config.js')
+const PROJECT_CONFIG_PATH_JS = path.resolve(process.cwd(), 'jamsocket.config.js')
+const PROJECT_CONFIG_PATH_JSON = path.resolve(process.cwd(), 'jamsocket.config.json')
 
 type ProjectConfig = {
   dockerfile?: string,
@@ -29,6 +30,32 @@ function isProjectConfig(obj: any): obj is ProjectConfig {
   return true
 }
 
+function getProjectConfig(): ProjectConfig | null {
+  const hasJson = existsSync(PROJECT_CONFIG_PATH_JSON)
+  const hasJs = existsSync(PROJECT_CONFIG_PATH_JS)
+  if (hasJson && hasJs) {
+    throw new Error('Both jamsocket.config.js and jamsocket.config.json files exist. Please remove one of them.')
+  }
+
+  if (hasJson) {
+    const projectConfig = JSON.parse(readFileSync(PROJECT_CONFIG_PATH_JSON, 'utf8'))
+    if (!isProjectConfig(projectConfig)) {
+      throw new Error('Invalid jamsocket.config.json file. Please see https://docs.jamsocket.com/platform/dev-cli for more information.')
+    }
+    return projectConfig
+  }
+
+  // TODO: phase out JS support?
+  if (hasJs) {
+    const projectConfig = require(PROJECT_CONFIG_PATH_JS)
+    if (!isProjectConfig(projectConfig)) {
+      throw new Error('Invalid jamsocket.config.js file. Please see https://docs.jamsocket.com/platform/dev-cli for more information.')
+    }
+    return projectConfig
+  }
+  return null
+}
+
 export default class Dev extends Command {
   static description = 'Starts a local jamsocket dev server. You may configure the dev server with a jamsocket.config.js file in the current directory or by passing flags. (Flags take precedence over jamsocket.config.js)'
   static examples = [
@@ -37,7 +64,10 @@ export default class Dev extends Command {
   ]
 
   static flags = {
-    dockerfile: Flags.string({ char: 'd', description: 'Path to the session backend\'s Dockerfile' }),
+    // TODO: breaking change: remove this and only use -f instead of -d
+    dockerfileold: Flags.string({ char: 'd', description: 'Path to the session backend\'s Dockerfile', hidden: true }),
+    dockerfile: Flags.string({ char: 'f', description: 'Path to the session backend\'s Dockerfile' }),
+    context: Flags.string({ char: 'c', description: 'Path to the build context for the Dockerfile (defaults to current working directory)' }),
     watch: Flags.string({ char: 'w', multiple: true, description: 'A file or directory to watch for changes' }),
     port: Flags.integer({ char: 'p', description: 'The port to run the dev server on. (Defaults to 8080)' }),
     interactive: Flags.boolean({ char: 'i', description: 'Enables/Disables TTY iteractivity. (Defaults to true)', allowNo: true }),
@@ -46,15 +76,8 @@ export default class Dev extends Command {
   public async run(): Promise<void> {
     const { flags } = await this.parse(Dev)
 
-    let projectConfig: ProjectConfig | null = null
-    if (existsSync(PROJECT_CONFIG_PATH)) {
-      projectConfig = require(PROJECT_CONFIG_PATH)
-      if (!isProjectConfig(projectConfig)) {
-        throw new Error('Invalid jamsocket.config.js file. Please see https://docs.jamsocket.com/platform/dev-cli for more information.')
-      }
-    }
-
-    const dockerfile = flags.dockerfile ?? projectConfig?.dockerfile ?? null
+    const projectConfig = getProjectConfig()
+    const dockerfile = flags.dockerfile ?? flags.dockerfileold ?? projectConfig?.dockerfile ?? null
     if (!dockerfile) {
       throw new Error('No Dockerfile provided. Please provide a Dockerfile with the --dockerfile flag or a jamsocket.config.js file in the current directory.')
     }
@@ -68,9 +91,10 @@ export default class Dev extends Command {
 
     const interactive = flags.interactive ?? projectConfig?.interactive ?? undefined
 
+    const dockerContext = flags.context ?? projectConfig?.dockerOptions?.path ?? undefined
     const dockerOptions: BuildImageOptions = {}
-    if (projectConfig?.dockerOptions?.path) {
-      dockerOptions.path = path.resolve(process.cwd(), projectConfig.dockerOptions.path)
+    if (dockerContext) {
+      dockerOptions.path = path.resolve(process.cwd(), dockerContext)
     }
 
     await createDevServer({
