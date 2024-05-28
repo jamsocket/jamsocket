1026c4b84de152d00bd654e638d7642ffd8ab628
Taylor Baldwin <taylorbaldwin@gmail.com>
2024-05-28 16:32:57 -0400

    link to dashboard from service/backend info and spawn commands

--- cli/src/commands/backend/info.ts
+++ cli/src/commands/backend/info.ts
@@ -1,7 +1,7 @@
 import { Command } from '@oclif/core'
 import chalk from 'chalk'
 import { Jamsocket } from '../../jamsocket'
-import { blue, lightBlue, lightMagenta } from '../../formatting'
+import { blue, lightBlue, lightMagenta, lightGreen } from '../../formatting'
 
 export default class Info extends Command {
   static description = 'Retrieves information about a backend given its name.'
@@ -17,6 +17,8 @@ export default class Info extends Command {
     const jamsocket = Jamsocket.fromEnvironment()
     const info = await jamsocket.backendInfo(args.backend)
 
+    const appBaseUrl = jamsocket.api.getAppBaseUrl()
+
     this.log(chalk.bold`Info for backend: ${lightMagenta(info.name)}`)
     this.log(`created:      ${blue(info.created_at)}`)
     this.log(`service:      ${blue(info.service_name)}`)
@@ -26,6 +28,7 @@ export default class Info extends Command {
     if (info.lock) this.log(`lock:         ${blue(info.lock)}`)
     if (info.environment_name) this.log(`environment:  ${blue(info.environment_name)}`)
     if (info.max_mem_bytes) this.log(`mem usage:    ${blue(`${info.max_mem_bytes} bytes`)}`)
+    this.log(`dashboard:    ${lightGreen(`${appBaseUrl}/backend/${info.name}`)}`)
     this.log()
     this.log(chalk.bold`Statuses:`)
     if (info.statuses.length === 0) {
