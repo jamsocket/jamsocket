d2509b04276795a33f62e39b4a442a6bc8b014d7
Taylor Baldwin <taylorbaldwin@gmail.com>
2024-08-13 17:30:57 -0400

    update multiplayer-editor example to work with updates to typescript lib

--- examples/multiplayer-editor/src/app/page.tsx
+++ examples/multiplayer-editor/src/app/page.tsx
@@ -1,13 +1,13 @@
 import 'server-only';
 import HomeContainer from '../components/Home'
-import { init } from '@jamsocket/server'
+import { Jamsocket } from '@jamsocket/server'
 
-const WHITEBOARD_NAME = 'multiplayer-editor-demo/default'
+const WHITEBOARD_NAME = 'my-whiteboard-room'
 
-const spawnBackend = init({ dev: true })
+const jamsocket = new Jamsocket({ dev: true })
 
 // In production, you'll want to do the following instead:
-// const spawnBackend = init({
+// const jamsocket = new Jamsocket({
 //   account: '[YOUR ACCOUNT HERE]',
 //   service: 'whiteboard-demo',
 //   // NOTE: we want to keep the Jamsocket token secret, so we can only do this in a server component
@@ -15,6 +15,6 @@ const spawnBackend = init({ dev: true })
 // })
 
 export default async function Page() {
-  const spawnResult = await spawnBackend({ lock: WHITEBOARD_NAME })
-  return <HomeContainer spawnResult={spawnResult} />
+  const connectResponse = await jamsocket.connect({ key: WHITEBOARD_NAME })
+  return <HomeContainer connectResponse={connectResponse} />
 }
