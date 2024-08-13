512a835ea10c65dce75924d77aa7b2c8490ce292
Taylor Baldwin <taylorbaldwin@gmail.com>
2024-08-13 17:30:35 -0400

    update server package (replace spawn() with connect())

--- packages/typescript/server/src/main.ts
+++ packages/typescript/server/src/main.ts
@@ -1,13 +1,13 @@
 import 'isomorphic-fetch' // fetch polyfill for older versions of Node
-
-export type SpawnResult = {
-  url: string
-  name: string
-  readyUrl: string
-  statusUrl: string
-  spawned: boolean
-  status: string
-}
+import type { ConnectRequest, ConnectResponse } from '@jamsocket/types'
+export type {
+  BackendStatus,
+  TerminationKind,
+  TerminationReason,
+  BackendState,
+  ConnectResponse,
+  ConnectRequest,
+} from '@jamsocket/types'
 
 export type JamsocketDevInitOptions = {
   dev: true
@@ -23,22 +23,6 @@ export type JamsocketInitOptions =
     }
   | JamsocketDevInitOptions
 
-export type JamsocketSpawnOptions = {
-  tag?: string
-  lock?: string
-  env?: Record<string, string>
-  gracePeriodSeconds?: number
-  serviceEnvironment?: string
-}
-
-type JamsocketApiSpawnBody = {
-  tag?: string
-  lock?: string
-  env?: Record<string, string>
-  grace_period_seconds?: number
-  service_environment?: string
-}
-
 const JAMSOCKET_DEV_PORT = 8080
 const JAMSOCKET_API = 'https://api.jamsocket.com'
 
@@ -53,75 +37,50 @@ function validatePort(port: any): number {
   return port
 }
 
-export type JamsocketInstance = {
-  /**
-   * @deprecated Calling a `JamsocketInstance` as a function is deprecated. Use the `spawn` method instead.
-   */
-  (opts: JamsocketSpawnOptions): Promise<SpawnResult>
-  spawn(opts: JamsocketSpawnOptions): Promise<SpawnResult>
-}
+export class Jamsocket {
+  account: string
+  service: string
+  token: string
+  apiUrl: string
 
-export function init(opts: JamsocketInitOptions): JamsocketInstance {
-  let account: string
-  let token: string
-  let service: string
-  let apiUrl: string
-
-  if (isJamsocketDevInitOptions(opts)) {
-    account = '-'
-    token = '-'
-    service = '-'
-    const port = opts.port ? validatePort(opts.port) : JAMSOCKET_DEV_PORT
-    apiUrl = `http://localhost:${port}`
-  } else {
-    account = opts.account
-    token = opts.token
-    service = opts.service
-    apiUrl = opts.apiUrl || JAMSOCKET_API
+  constructor(opts: JamsocketInitOptions) {
+    if (isJamsocketDevInitOptions(opts)) {
+      this.account = '-'
+      this.token = '-'
+      this.service = '-'
+      const port = opts.port ? validatePort(opts.port) : JAMSOCKET_DEV_PORT
+      this.apiUrl = `http://localhost:${port}`
+    } else {
+      this.account = opts.account
+      this.token = opts.token
+      this.service = opts.service
+      this.apiUrl = opts.apiUrl || JAMSOCKET_API
+    }
   }
 
-  const spawnInner = async function (spawnOpts: JamsocketSpawnOptions = {}): Promise<SpawnResult> {
-    const reqBody: JamsocketApiSpawnBody = {}
-    if (spawnOpts.tag) reqBody.tag = spawnOpts.tag
-    if (spawnOpts.lock) reqBody.lock = spawnOpts.lock
-    if (spawnOpts.env) reqBody.env = spawnOpts.env
-    if (spawnOpts.gracePeriodSeconds) reqBody.grace_period_seconds = spawnOpts.gracePeriodSeconds
-    if (spawnOpts.serviceEnvironment) reqBody.service_environment = spawnOpts.serviceEnvironment
-
-    const response = await fetch(`${apiUrl}/user/${account}/service/${service}/spawn`, {
-      method: 'POST',
-      headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
-      body: JSON.stringify(reqBody),
-      cache: 'no-store',
-    })
+  async connect(connectRequest?: ConnectRequest): Promise<ConnectResponse> {
+    const response = await fetch(
+      `${this.apiUrl}/v2/service/${this.account}/${this.service}/connect`,
+      {
+        method: 'POST',
+        headers: { Authorization: `Bearer ${this.token}`, 'Content-Type': 'application/json' },
+        body: JSON.stringify(connectRequest || {}),
+        cache: 'no-store',
+      },
+    )
+    const bodyText = await response.text()
     if (!response.ok) {
       if (response.status === 429) {
         console.warn(
           "You've hit the spawn rate limit. This may be because you've spawned too many session backends in a short period of time or you're already running the maximum number of concurrent session backends. (related: https://docs.jamsocket.com/pricing/free-tier-limits)",
         )
       }
-      throw new Error(`Error spawning backend: ${response.status} ${await response.text()}`)
+      throw new Error(`Error spawning backend: ${response.status} ${bodyText}`)
     }
-    const body = await response.json()
-    return {
-      url: body.url,
-      name: body.name,
-      readyUrl: body.ready_url,
-      statusUrl: body.status_url,
-      spawned: body.spawned,
-      status: body.status,
+    try {
+      return JSON.parse(bodyText) as ConnectResponse
+    } catch (e) {
+      throw new Error(`Error parsing connect response: ${bodyText}`)
     }
   }
-
-  const spawn = async function (spawnOpts: JamsocketSpawnOptions = {}): Promise<SpawnResult> {
-    console.warn(
-      'Calling the result of Jamsocket.init(...)() directly is deprecated, call Jamsocket.init().spawn(...) instead.',
-    )
-    return spawnInner(spawnOpts)
-  }
-
-  spawn.spawn = spawnInner
-  return spawn
 }
-
-export default { init }
